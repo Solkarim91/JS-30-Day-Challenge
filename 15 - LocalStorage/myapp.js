4c77188656adba5aa9c14f3bb57dc61b1d306641
Solomon Karim <93667920+Solkarim91@users.noreply.github.com>
2022-05-08 16:20:09 +0100

    setup files

---
+++ 15 - LocalStorage/myapp.js
@@ -0,0 +1,20 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+  items.push(item);
+  this.reset();
+};
+
+function populateList(plates = [], platesList) {
+
+};
+
+addItems.addEventListener('submit', addItem);
